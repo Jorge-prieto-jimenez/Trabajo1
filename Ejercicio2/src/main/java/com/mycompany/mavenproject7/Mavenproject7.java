fb526a5d8b47eb51c9873ec92ddd34accf3bcdf8
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2023-03-04 22:12:16 -0500

    p

---
+++ Ejercicio2/src/main/java/com/mycompany/mavenproject7/Mavenproject7.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject7;
+
+
+public class Mavenproject7 {
+
+    public static void main(String[] args) {
+        double suma,x,y;
+        
+        suma = 0;
+        x = 20;
+        suma = suma+x;
+        y = 40;
+        x = x + Math.pow(y, 2);
+        suma = suma + x/y;
+        
+        System.out.println("El valor de las uma es: "+suma);
+        
+                
+        
+    }
+}
